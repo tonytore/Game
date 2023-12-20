f3ceebb686fbe5ebbdc7a431d748643ba6bcc44a
tonytor <teme4842@gmail.com>
2023-12-20 08:27:19 +0300

    pretty level transition(update the current transition)

--- level/out_side.gd
+++ level/out_side.gd
@@ -4,4 +4,4 @@ extends LevelParent
 func _on_gate_player_crossed_gate():
 	var tween = create_tween()
 	tween.tween_property($Player,"speed",0,0.5)
-	get_tree().change_scene_to_file("res://scene/level/insides.tscn")
+	TransitionLayer.change_scene("res://scene/level/insides.tscn")
